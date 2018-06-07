c73957717dab31e409205d7cab16a5e3121eeec1
ortizmarcosn <ortizmarcosn@gmail.com>
2018-06-07 19:03:26 -0300

    Add files via upload

---
+++ WindowsFormsApplication2/ListadoEstadistico/ListadoEstadistico.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class ListadoEstadistico
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbTipoEstadistica = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbTrimestre = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbTipoListado = new System.Windows.Forms.ComboBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dtpAnio = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(147, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 18);
+            this.label1.TabIndex = 51;
+            this.label1.Text = "Trimestre";
+            // 
+            // cbTipoEstadistica
+            // 
+            this.cbTipoEstadistica.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbTipoEstadistica.FormattingEnabled = true;
+            this.cbTipoEstadistica.Items.AddRange(new object[] {
+            "Hoteles con mayor cantidad de reservas canceladas.",
+            "Hoteles con mayor cantidad de consumibles facturados.",
+            "Hoteles con mayor cantidad de días fuera de servicio.",
+            "Habitaciones con mayor cantidad de días y veces ocupadas.",
+            "Cliente con mayor cantidad de puntos."});
+            this.cbTipoEstadistica.Location = new System.Drawing.Point(150, 18);
+            this.cbTipoEstadistica.Name = "cbTipoEstadistica";
+            this.cbTipoEstadistica.Size = new System.Drawing.Size(553, 21);
+            this.cbTipoEstadistica.TabIndex = 52;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 18);
+            this.label2.TabIndex = 53;
+            this.label2.Text = "Año";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(471, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 18);
+            this.label3.TabIndex = 54;
+            this.label3.Text = "Tipo listado";
+            // 
+            // cbTrimestre
+            // 
+            this.cbTrimestre.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbTrimestre.FormattingEnabled = true;
+            this.cbTrimestre.Items.AddRange(new object[] {
+            "Enero - Febrero - Marzo",
+            "Abril - Mayo - Junio",
+            "Julio - Agosto - Septiembre",
+            "Octubre - Noviembre - Diciembre"});
+            this.cbTrimestre.Location = new System.Drawing.Point(226, 58);
+            this.cbTrimestre.Name = "cbTrimestre";
+            this.cbTrimestre.Size = new System.Drawing.Size(226, 21);
+            this.cbTrimestre.TabIndex = 55;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(27, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 18);
+            this.label4.TabIndex = 56;
+            this.label4.Text = "Tipo estadística";
+            // 
+            // cbTipoListado
+            // 
+            this.cbTipoListado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbTipoListado.FormattingEnabled = true;
+            this.cbTipoListado.Items.AddRange(new object[] {
+            "Simple",
+            "Detallado"});
+            this.cbTipoListado.Location = new System.Drawing.Point(569, 58);
+            this.cbTipoListado.Name = "cbTipoListado";
+            this.cbTipoListado.Size = new System.Drawing.Size(134, 21);
+            this.cbTipoListado.TabIndex = 57;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnSalir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSalir.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(430, 111);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(116, 32);
+            this.btnSalir.TabIndex = 59;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnBuscar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBuscar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(226, 111);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(117, 32);
+            this.btnBuscar.TabIndex = 60;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.dtpAnio);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.btnBuscar);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnSalir);
+            this.panel1.Controls.Add(this.cbTipoEstadistica);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.cbTipoListado);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.cbTrimestre);
+            this.panel1.Location = new System.Drawing.Point(61, 75);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(724, 171);
+            this.panel1.TabIndex = 61;
+            // 
+            // dtpAnio
+            // 
+            this.dtpAnio.CustomFormat = "yyyy";
+            this.dtpAnio.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpAnio.Location = new System.Drawing.Point(69, 55);
+            this.dtpAnio.Name = "dtpAnio";
+            this.dtpAnio.Size = new System.Drawing.Size(70, 20);
+            this.dtpAnio.TabIndex = 61;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(61, 289);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(724, 166);
+            this.dataGridView1.TabIndex = 62;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(58, 268);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 18);
+            this.label5.TabIndex = 63;
+            this.label5.Text = "TOP 5 Resultados";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(243, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(377, 46);
+            this.label6.TabIndex = 64;
+            this.label6.Text = "Listado estadístico";
+            // 
+            // ListadoEstadistico
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(834, 487);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "ListadoEstadistico";
+            this.Text = "ListadoEstadistico";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbTipoEstadistica;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbTrimestre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbTipoListado;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DateTimePicker dtpAnio;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ WindowsFormsApplication2/ListadoEstadistico/ListadoEstadistico.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class ListadoEstadistico : Form
+    {
+        public ListadoEstadistico()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult respuesta;
+
+            respuesta = MessageBox.Show("¿Desea usted salir?", "Salir del programa", MessageBoxButtons.YesNo, MessageBoxIcon.Stop);
+
+            if (respuesta == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if (ValidarDatosCompletos())
+            {
+                //Que haga la búsqueda
+            }
+            
+        }
+
+        private Boolean ValidarDatosCompletos()
+        {
+            String TipoEstadistica = this.cbTipoEstadistica.Text;
+            String Anio = this.dtpAnio.Text;
+            String Trimestre = this.cbTrimestre.Text;
+            String TipoListado = this.cbTipoListado.Text;
+
+            if (TipoEstadistica == "" || Anio == "" || Trimestre == "" || TipoListado == "")
+            {
+                MessageBox.Show("Complete todos los campos", "Campos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+    }
+}
