f3a3d8547e15e73d07836fd3fe9867a47cdcc17c
Julian Goldfarb <juligol_96@hotmail.com>
2018-06-18 00:07:05 -0300

    avance de las create table hasta reserva_clientes

--- sql/sript_creacion_inicial.sql
+++ sql/sript_creacion_inicial.sql
@@ -32,8 +32,8 @@ IF OBJECT_ID('PUNTOZIP.REGIMENES_HOTELES') IS NOT NULL
 DROP TABLE [PUNTOZIP].REGIMENES_HOTELES
 GO
 
-IF OBJECT_ID('PUNTOZIP.REGIMEN') IS NOT NULL
-DROP TABLE [PUNTOZIP].REGIMEN
+IF OBJECT_ID('PUNTOZIP.REGIMENES') IS NOT NULL
+DROP TABLE [PUNTOZIP].REGIMENES
 GO
 
 IF OBJECT_ID('PUNTOZIP.HABITACIONES') IS NOT NULL
@@ -94,5 +94,264 @@ DROP SCHEMA [PUNTOZIP]
 GO
 
 
-CREATE SCHEMA [PUNTO_ZIP]
+CREATE SCHEMA [PUNTOZIP]
+
+-------------------------------- Crear tablas ---------------------------------------
+
+-------------------------------- USUARIOS ---------------------------------------
 GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [PUNTOZIP].[USUARIOS](
+	[usu_id] [int] IDENTITY(1,1) NOT NULL,
+	[usu_username] [nvarchar](255) NOT NULL,
+	[usu_password] [nvarchar](255) NOT NULL,
+	[usu_nombre] [nvarchar](255) NOT NULL,
+	[usu_apellido] [nvarchar](255) NOT NULL,
+	[usu_mail] [nvarchar](255) NOT NULL,
+	[usu_tipo_documento] [nvarchar](255) NOT NULL,
+	[usu_documento] [nvarchar](255) NOT NULL,
+	[usu_telefono] [nvarchar](255) NOT NULL,
+	[usu_direccion] [nvarchar](255) NOT NULL,
+	[usu_fecha_nacimiento] [datetime] NOT NULL,
+	[usu_hotel_id] [int] NOT NULL
+	CONSTRAINT [PK_USUARIOS] PRIMARY KEY CLUSTERED 
+	( [usu_id] ASC )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+-------------------------------- ROLES ---------------------------------------
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [PUNTOZIP].[ROLES](
+	[rol_id] [int] IDENTITY(1,1) NOT NULL,
+	[rol_descripcion] [nvarchar](255) NOT NULL,
+	[rol_estado] [tinyint] NOT NULL
+	CONSTRAINT [PK_ROLES] PRIMARY KEY CLUSTERED 
+	( [rol_id] ASC )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+-------------------------------- ROLES_USUARIOS ---------------------------------------
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [PUNTOZIP].[ROLES_USUARIOS](
+	[ru_usuario_id] [int] NOT NULL,
+	[ru_rol_id] [int] NOT NULL
+	CONSTRAINT [PK_ROLES_USUARIOS] PRIMARY KEY CLUSTERED 
+	( [ru_usuario_id] ASC, [ru_rol_id] ASC )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+-------------------------------- FUNCIONES ---------------------------------------
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [PUNTOZIP].[FUNCIONES](
+	[func_id] [int] IDENTITY(1,1) NOT NULL,
+	[func_descripcion] [text] NOT NULL
+	CONSTRAINT [PK_FUNCIONES] PRIMARY KEY CLUSTERED 
+	( [func_id] ASC )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+-------------------------------- ROLES_FUNCIONES ---------------------------------------
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [PUNTOZIP].[ROLES_FUNCIONES](
+	[rf_rol_id] [int] NOT NULL,
+	[rf_funcion_id] [int] NOT NULL
+	CONSTRAINT [PK_ROLES_FUNCIONES] PRIMARY KEY CLUSTERED 
+	( [rf_rol_id] ASC, [rf_funcion_id] ASC )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+-------------------------------- HOTELES ---------------------------------------
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [PUNTOZIP].[HOTELES](
+	[hote_id] [int] IDENTITY(1,1) NOT NULL,
+	[hote_nombre] [nvarchar](255) NOT NULL,
+	[hote_mail] [nvarchar](255) NOT NULL,
+	[hote_telefono] [nvarchar](255) NOT NULL,
+	[hote_pais] [nvarchar](255) NOT NULL,
+	[hote_ciudad] [nvarchar](255) NOT NULL,
+	[hote_calle] [nvarchar](255) NOT NULL,
+	[hote_numero_calle] [numeric](18,0) NOT NULL,
+	[hote_cantidad_estrellas] [numeric](18,0) NOT NULL,
+	[hote_recarga_estrellas] [numeric](18,0) NOT NULL,
+	--[hote_regimen_tipo] [nvarchar](255) NOT NULL,
+	[hote_fecha_creacion] [datetime] NOT NULL,
+	--[hote_ubicacion_id] [int] NOT NULL
+	CONSTRAINT [PK_HOTELES] PRIMARY KEY CLUSTERED 
+	( [hote_id] ASC )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+-------------------------------- PERIODO_HOTELES ---------------------------------------
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [PUNTOZIP].[PERIODO_HOTELES](
+	[ph_id] [int] IDENTITY(1,1) NOT NULL,
+	[ph_fecha_inicio] [datetime] NOT NULL,
+	[ph_fecha_fin] [datetime] NOT NULL,
+	[ph_motivos] [text] NOT NULL,
+	[ph_hotel_id] [int] NOT NULL
+	CONSTRAINT [PK_PERIODO_HOTELES] PRIMARY KEY CLUSTERED 
+	( [ph_id] ASC )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+-------------------------------- REGIMENES ---------------------------------------
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [PUNTOZIP].[REGIMENES](
+	[regi_id] [int] IDENTITY(1,1) NOT NULL,
+	[regi_descripcion] [nvarchar](255) NOT NULL,
+	--[regi_codigo] [numeric](18,2) NOT NULL,
+	[regi_precio] [numeric](18,2) NOT NULL,
+	[regi_estado] [tinyint] NOT NULL
+	CONSTRAINT [PK_REGIMENES] PRIMARY KEY CLUSTERED 
+	( [regh_id] ASC )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+-------------------------------- REGIMENES_HOTELES ---------------------------------------
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [PUNTOZIP].[REGIMENES_HOTELES](
+	[regh_regimen_id] [int] NOT NULL,
+	[regh_hotel_id] [int] NOT NULL
+	CONSTRAINT [PK_REGIMENES_HOTELES] PRIMARY KEY CLUSTERED 
+	( [regh_regimen_id] ASC, [regh_hotel_id] ASC )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+-------------------------------- HABITACIONES ---------------------------------------
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [PUNTOZIP].[HABITACIONES](
+	[habi_id] [int] IDENTITY(1,1) NOT NULL,
+	[habi_descripcion] [nvarchar](255) NOT NULL,
+	[habi_piso] [numeric](18,2) NOT NULL,
+	[habi_numero] [numeric](18,2) NOT NULL,
+	[habi_tipo_id] [int] NOT NULL,
+	[habi_vista_tipo_id] [int] NOT NULL,
+	[habi_estado] [tinyint] NOT NULL,
+	[habi_hotel_id] [int] NOT NULL
+	CONSTRAINT [PK_HABITACIONES] PRIMARY KEY CLUSTERED 
+	( [habi_id] ASC )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+-------------------------------- TIPO_HABITACION ---------------------------------------
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [PUNTOZIP].[TIPO_HABITACION](
+	[th_id] [int] IDENTITY(1,1) NOT NULL,
+	[th_descripcion] [nvarchar](255) NOT NULL,
+	[th_porcentual] [numeric](18,2) NOT NULL
+	CONSTRAINT [PK_TIPO_HABITACION] PRIMARY KEY CLUSTERED 
+	( [th_id] ASC )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+-------------------------------- VISTA_HOTEL ---------------------------------------
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [PUNTOZIP].[VISTA_HOTEL](
+	[vh_id] [int] IDENTITY(1,1) NOT NULL,
+	[vh_descripcion] [nvarchar](255) NOT NULL
+	CONSTRAINT [PK_VISTA_HOTEL] PRIMARY KEY CLUSTERED 
+	( [vh_id] ASC )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+-------------------------------- RESERVAS ---------------------------------------
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [PUNTOZIP].[RESERVAS](
+	[rese_id] [int] IDENTITY(1,1) NOT NULL,
+	[rese_fecha_desde] [datetime] NOT NULL,
+	[rese_fecha_hasta] [datetime] NOT NULL,
+	[rese_codigo] [numeric](18,0) NOT NULL,
+	[rese_estado] [tinyint] NOT NULL
+	CONSTRAINT [PK_RESERVAS] PRIMARY KEY CLUSTERED 
+	( [rese_id] ASC )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+-------------------------------- RESERVAS_HABITACIONES ---------------------------------------
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [PUNTOZIP].[RESERVAS_HABITACIONES](
+	[rh_reserva_id] [int] NOT NULL,
+	[rh_habitacion_id] [int] NOT NULL
+	CONSTRAINT [PK_RESERVAS_HABITACIONES] PRIMARY KEY CLUSTERED 
+	( [rh_reserva_id] ASC, [rh_habitacion_id] ASC )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+-------------------------------- CLIENTES ---------------------------------------
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [PUNTOZIP].[CLIENTES](
+	[clie_id] [int] IDENTITY(1,1) NOT NULL,
+	[clie_nombre] [nvarchar](255) NOT NULL,
+	[clie_apellido] [nvarchar](255) NOT NULL,
+	[clie_tipo_identificacion] [nvarchar](255) NOT NULL,
+	[clie_numero_identificacion] [nvarchar](255) NOT NULL,
+	[clie_numero_pasaporte] [numeric](18,0) NOT NULL,
+	[clie_mail] [nvarchar](255) NOT NULL,
+	[clie_domicilio_calle] [nvarchar](255) NOT NULL,
+	[clie_numero_calle] [numeric](18,0) NOT NULL,
+	[clie_piso] [numeric](18,0) NOT NULL,
+	[clie_nacionalidad] [nvarchar](255) NOT NULL,
+	[clie_fecha_nacimiento] [datetime] NOT NULL,
+	[clie_estado] [tinyint] NOT NULL
+	CONSTRAINT [PK_CLIENTES] PRIMARY KEY CLUSTERED 
+	( [clie_id] ASC )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+-------------------------------- RESERVAS_CLIENTES ---------------------------------------
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [PUNTOZIP].[RESERVAS_CLIENTES](
+	[rc_reserva_id] [int] NOT NULL,
+	[rc_cliente_id] [int] NOT NULL,
+	[rc_cantidad_noches] [numeric](18,0) NOT NULL
+	CONSTRAINT [PK_RESERVAS_CLIENTES] PRIMARY KEY CLUSTERED 
+	( [rh_reserva_id] ASC, [rc_cliente_id] ASC )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
