050b76adf282162461b5602d8a3fdd60bfb8076e
nburza <nicoburza_96@hotmail.com>
2018-06-19 16:27:00 -0300

    Corrijo cadena de conexion

--- WindowsFormsApplication2/Properties/Settings.Designer.cs
+++ WindowsFormsApplication2/Properties/Settings.Designer.cs
@@ -26,8 +26,8 @@ namespace WindowsFormsApplication2.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=NICO\\SQLSERVER2012;Initial Catalog=GD1C2018;Persist Security Info=Tru" +
-            "e;User ID=gdHotel2018;Password=gd2018")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=localhost\\SQLSERVER2012;Initial Catalog=GD1C2018;Persist Security Inf" +
+            "o=True;User ID=gdHotel2018;Password=gd2018")]
         public string Conexion {
             get {
                 return ((string)(this["Conexion"]));
